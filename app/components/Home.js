a0ea84e861d8015a9b7e53cdeb8d40d74bffbd76
felix <kid2683@gmail.com>
2018-10-30 22:56:49 +0800

    retry

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,28 +1,34 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ip from 'ip';
 import styles from './Home.css';
-import ip from 'ip'
-const QRCode = require('qrcode')
+
+const QRCode = require('qrcode');
+
 type Props = {};
 
 export default class Home extends Component<Props> {
-  componentDidMount () {
-    console.log('http://' + ip.address() + ':8090');
-    QRCode.toCanvas(document.getElementById('test'), 'http://' + ip.address() + ':8088', function (error) {
-      if (error) console.error(error)
-      console.log('success!')
-    })
-  }
   props: Props;
+
+  componentDidMount() {
+    const url = `http://${ip.address()}:8090`;
+    console.log(url);
+    QRCode.toCanvas(document.getElementById('test'), url, error => {
+      if (error) console.error(error);
+      console.log('success!');
+    });
+  }
 
   render() {
     return (
       <div>
+        <div className={styles.header}>
+          <canvas id="test" />
+        </div>
         <div className={styles.container} data-tid="container">
           <h2>Home1</h2>
           <Link to="/counter">to Counter</Link>
-          <canvas id="test"></canvas>
         </div>
       </div>
     );
